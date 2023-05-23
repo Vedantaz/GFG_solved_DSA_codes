405a51c9619ce128d7bda97ffc4fc990c3e9a629
Vedantaz <muleyvedant4@gmail.com>
2023-05-23 19:52:02 +0530

    added heap codes

--- heap/kth largest element jin a astream.cpp
+++ heap/kth largest element jin a astream.cpp
@@ -1,4 +1,4 @@
-priority_queue<int,vector<int>,greater<int>>pq;
+        priority_queue<int,vector<int>,greater<int>>pq;
         vector<int>v;
         for(int i=0; i<n; i++){
             pq.push(arr[i]);
